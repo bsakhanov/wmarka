96754bd391f8cd115f9aca7115d2c40aeaeac0f9
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2023-08-10 09:58:40 +0600

    Update component.php

--- component.php
+++ component.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * J!Blank Template for Joomla by JBlank.pro (JBZoo.com)
- *
- * @package    JBlank
- * @author     SmetDenis <admin@jbzoo.com>
- * @copyright  Copyright (c) JBlank.pro
- * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
- * @link       http://jblank.pro/ JBlank project page
- */
-
 defined('_JEXEC') or die;
 
 
