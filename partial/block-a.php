7719464b1bb39b1f9eb422e6ec72aa22f02c5797
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 17:09:46 +0500

    Update block-a.php
    более точный и легкий стиль

--- partial/block-a.php
+++ partial/block-a.php
@@ -1,19 +1,43 @@
 <?php
-defined('_JEXEC') or die;
+/**
+ * Ультра-лаконичный универсальный шаблон секции для Joomla 6
+ * Использует имя позиции в качестве ID напрямую.
+ */
 
-use Joomla\CMS\Factory;
+defined('_JEXEC') or die;
 
 /** @var \Wmarka\Template\Helper $this */
 $doc = $this->doc;
 
-// Проверяем наличие модулей в позиции, чтобы не плодить пустые теги в DOM
-if ($doc->countModules('block-a')) : ?>
-    <section id="section-block-a" class="uk-section uk-section-default">
+// --- ИМЯ ПОЗИЦИИ ---
+// Просто меняй это значение для каждого нового файла в папке /partial/
+$position = 'block-a'; 
+
+// Проверка: если в позиции нет модулей, секция не рендерится вовсе
+if ($doc->countModules($position)) : ?>
+
+    <?php /* 
+       Контейнер секции: uk-section-default — белый фон. Можно менять на muted, primary, secondary или любой кастомный.
+       id — полезен для якорных ссылок (меню -> #section-block-a)
+    */ ?>
+    <section id="<?php echo $position; ?>" class="uk-section uk-section-default">
+        
         <div class="uk-container">
             
-            <?php /* Вывод модулей с нашим новым стилем хрома */ ?>
-            <jdoc:include type="modules" name="block-a" style="wmarka" />
+            <?php /* Сетка UIkit 3:
+               - uk-grid-match: делает все модули в ряду одной высоты.
+               - uk-child-width-*: управляет колонками (1 на мобиле, 2 на планшете, 3 на ПК).
+            */ ?>
+            <div class="uk-grid-medium uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-match" uk-grid>
+                
+                <?php /* Твой стиль wmarka из html/modules.php.
+                   Все модули станут элементами этой сетки автоматически.
+                */ ?>
+                <jdoc:include type="modules" name="<?php echo $position; ?>" style="wmarka" />
+                
+            </div>
             
         </div>
     </section>
+
 <?php endif; ?>
