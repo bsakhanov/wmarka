9ad33b5bf42f671ef7c69fb1b68f72c2770eccfd
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 20:11:20 +0500

    Update raw.php

--- raw.php
+++ raw.php
@@ -1,15 +1,36 @@
 <?php
 /**
- * J!Blank Template for Joomla by JBlank.pro (JBZoo.com)
- *
- * @package    JBlank
- * @author     SmetDenis <admin@jbzoo.com>
- * @copyright  Copyright (c) JBlank.pro
- * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
- * @link       http://jblank.pro/ JBlank project page
+ * Файл вывода чистого компонента (версия для печати / модальные окна)
+ * Joomla 6 + UIkit 3
  */
 
 defined('_JEXEC') or die;
 
+// Инициализация хелпера шаблона (подключает стили и скрипты)
+require_once dirname(__FILE__) . '/php/init.php';
 
-?><jdoc:include type="component" />
+/** @var \Wmarka\Template\Helper $tpl */
+$doc = $this->doc;
+?>
+
+<!DOCTYPE html>
+<?php echo $tpl->renderHTML(); ?>
+<head>
+    <?php /* Подключаем системные мета-теги и ассеты из joomla.asset.json */ ?>
+    <jdoc:include type="head" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="<?php echo $tpl->getBodyClasses(); ?> uk-background-white">
+    
+    <main class="uk-section uk-section-small">
+        <div class="uk-container">
+            
+            <?php /* Вывод основного содержимого компонента */ ?>
+            <jdoc:include type="component" />
+            
+        </div>
+    </main>
+
+</body>
+</html>
