71aeff7fa8a7056f48e81d6121dfc060508e165d
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 15:28:19 +0500

    Update toolbar.php

--- partial/toolbar.php
+++ partial/toolbar.php
@@ -1,26 +1,21 @@
 <?php
 defined('_JEXEC') or die;
-use Joomla\CMS\Language\Text;
 
-/*
- * toolbar-left
- * toolbar-right
- */
-if ($_this->countModules('toolbar-left') || $_this->countModules('toolbar-right')) {
-    ?>
+// Проверяем наличие модулей через объект документа
+if ($this->doc->countModules('toolbar-left') || $this->doc->countModules('toolbar-right')) : ?>
 <div role="toolbar" id="section-toolbar" class="uk-section uk-section-xsmall uk-section-primary">
     <div class="uk-container">
         <div class="uk-flex uk-flex-middle uk-flex-between">
 
-            <?php if ($_this->countModules('toolbar-left')) { ?>
-            <jdoc:include type="modules" name="toolbar-left" style="master3" />
-            <?php } ?>
+            <?php if ($this->doc->countModules('toolbar-left')) : ?>
+                <jdoc:include type="modules" name="toolbar-left" style="master3" />
+            <?php endif; ?>
 
-            <?php if ($_this->countModules('toolbar-right')) { ?>
-            <jdoc:include type="modules" name="toolbar-right" style="master3" />
-            <?php } ?>
+            <?php if ($this->doc->countModules('toolbar-right')) : ?>
+                <jdoc:include type="modules" name="toolbar-right" style="master3" />
+            <?php endif; ?>
 
         </div>
     </div>
 </div>
-<?php } ?>
+<?php endif; ?>
