068a5e685a0a242c6adffefb0a5effb774c29368
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 17:27:09 +0500

    Update data-markup.php

--- partial/data-markup.php
+++ partial/data-markup.php
@@ -1,35 +1,50 @@
-<?php $itemid = JRequest::getVar('Itemid');
-if($itemid == '169' || $itemid == '208' || $itemid == '259' || $itemid == '361'): // для главной, для компании, для контактов ?>
-<!-- microdata -->
-<script type="application/ld+json">
-{
-  "@context" : "http://schema.org",
-  "@type" : "LocalBusiness",
-  "name" : "Студия Webmarka Kazakhstan",
-  "image" : "https://webmarka.kz/images/zfavicons/apple-touch-icon.png",
-  "telephone" : [ "+7 (7172) 251-394", "+7 (708) 4251-394" ],
-  "email" : "support@webmarka.kz",
-  "priceRange" : "180000-1000000KZT",
-  "address" : {
-    "@type" : "PostalAddress",
-    "streetAddress" : "ул. Касыма Аманжолова 32/1",
-    "addressLocality" : "Астана",
-    "addressCountry" : "Казахстан",
-    "postalCode" : "010000"
-  },
-  "openingHoursSpecification" : {
-    "@type" : "OpeningHoursSpecification",
-    "dayOfWeek" : {
-      "@type" : "DayOfWeek",
-      "name" : "пн,вт, ср, чт, пт, сб"
-    },
-    "opens": "09:00",
-    "closes": "21:00"
-  },
-  "url" : "https://webmarka.kz/"
-}
-</script>
+<?php
+/**
+ * Разметка Schema.org LocalBusiness
+ * Выводится только на определенных страницах (Главная, О компании, Контакты)
+ */
 
-<!-- /microdata -->
-<?php endif; ?>
+defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+
+// Получаем текущий Itemid через современный API
+$app    = Factory::getApplication();
+$itemId = $app->input->getInt('Itemid');
+
+// Массив ID страниц, где нужна эта разметка
+$targetIds = [169, 208, 259, 361];
+
+// Проверяем, входит ли текущий ID в наш список
+if (in_array($itemId, $targetIds)) : ?>
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "LocalBusiness",
+        "name": "Студия Webmarka Kazakhstan",
+        "image": "https://webmarka.kz/images/zfavicons/apple-touch-icon.png",
+        "telephone": [
+            "+7 (7172) 251-394",
+            "+7 (708) 4251-394"
+        ],
+        "email": "support@webmarka.kz",
+        "priceRange": "180000-1000000KZT",
+        "url": "https://webmarka.kz/",
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "ул. Касыма Аманжолова 32/1",
+            "addressLocality": "Астана",
+            "addressCountry": "Казахстан",
+            "postalCode": "010000"
+        },
+        "openingHoursSpecification": {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": [
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+            ],
+            "opens": "09:00",
+            "closes": "21:00"
+        }
+    }
+    </script>
+    <?php endif; ?>
