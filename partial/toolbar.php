f6682910e816873b83be21e15a61db2835b986e1
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 15:36:57 +0500

    Update toolbar.php
    адаптация под Joomla 6

--- partial/toolbar.php
+++ partial/toolbar.php
@@ -1,6 +1,15 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
+
+/** @var \Wmarka\Template\Helper $this */
+$doc = $this->doc;
+$app = Factory::getApplication(); // Теперь Factory всегда под рукой
+
 // Проверяем наличие модулей через объект документа
 if ($this->doc->countModules('toolbar-left') || $this->doc->countModules('toolbar-right')) : ?>
 <div role="toolbar" id="section-toolbar" class="uk-section uk-section-xsmall uk-section-primary">
@@ -8,11 +17,11 @@ if ($this->doc->countModules('toolbar-left') || $this->doc->countModules('toolba
         <div class="uk-flex uk-flex-middle uk-flex-between">
 
             <?php if ($this->doc->countModules('toolbar-left')) : ?>
-                <jdoc:include type="modules" name="toolbar-left" style="master3" />
+                <jdoc:include type="modules" name="toolbar-left" style="wmarka" />
             <?php endif; ?>
 
             <?php if ($this->doc->countModules('toolbar-right')) : ?>
-                <jdoc:include type="modules" name="toolbar-right" style="master3" />
+                <jdoc:include type="modules" name="toolbar-right" style="wmarka" />
             <?php endif; ?>
 
         </div>
