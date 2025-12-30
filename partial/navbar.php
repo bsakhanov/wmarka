fd5035945e944ba61b4009f121fb4a881c43a95b
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 15:42:41 +0500

    Update navbar.php
    адаптация под Joomla 6

--- partial/navbar.php
+++ partial/navbar.php
@@ -1,106 +1,80 @@
 <?php
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Filesystem\Path;
 use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\CMS\Language\Text;
 use Joomla\Module\Finder\Site\Helper\FinderHelper;
 
-/*
- * navbar-left
- * navbar-center
- * navbar-right
- */
+$doc = $this->doc;
+$isHome = Uri::getInstance()->toString() === Uri::base(); // Проверка главной страницы
 
-if (
-    $_this->countModules('navbar-left')
-    || $_this->countModules('navbar-center')
-    || $_this->countModules('navbar-right')
-    || $_this->countModules('lang')	
-) {
-    ?>
-<div role="navigation" id="navbar" class="uk-section uk-padding-remove-vertical uk-margin-remove-vertical uk-navbar-container"  itemscope itemtype="http://www.schema.org/SiteNavigationElement" uk-sticky>	
-    <div class="uk-container">
-        <div data-uk-navbar>
-            <?php if ($_this->countModules('navbar-left')) { ?>
-            <div class="uk-navbar-left uk-hidden@m">
-                <a href="#offcanvas-menu" class="uk-navbar-toggle"
-                    data-uk-navbar-toggle-icon data-uk-toggle aria-label="Menu"></a>
-            </div>
-            <div class="uk-navbar-left uk-visible@m">
-                <jdoc:include type="modules" name="navbar-left" style="master3" />
-            </div>
-            <?php } ?>
+if ($doc->countModules('navbar-left | navbar-center | navbar-right | iconnav')) : ?>
+<div class="uk-section-brand uk-preserve-color uk-margin-bottom">
+    <nav role="navigation" id="navbar" 
+         class="uk-navbar-container uk-background-brand uk-menu" 
+         uk-sticky="cls-active: uk-navbar-sticky; show-on-up: true; animation: uk-animation-slide-top">
+        
+        <div class="uk-container">
+            <div uk-navbar>
 
-            <?php if ($_this->countModules('navbar-center')) { ?>
-            <div class="uk-navbar-center uk-hidden@m">
-                <a href="#offcanvas-menu" class="uk-navbar-toggle"
-                    data-uk-navbar-toggle-icon data-uk-toggle aria-label="Menu"></a>
-            </div>
-            <div class="uk-navbar-center uk-visible@m">
-                <jdoc:include type="modules" name="navbar-center" style="master3" />
-            </div>
-            <?php } ?>
+                <div class="uk-navbar-left">
+                    <?php if (!$isHome) : ?>
+                        <a href="<?php echo Uri::base(); ?>" class="uk-navbar-item uk-logo">
+                            <span uk-icon="home"></span>
+                        </a>
+                    <?php endif; ?>
+                    
+                    <jdoc:include type="modules" name="navbar-left" style="wmarka" />
+                </div>
 
-            <?php if ($_this->countModules('navbar-right')) { ?>
-            <div class="uk-navbar-right uk-hidden@m">
-                <a href="#offcanvas-menu" class="uk-navbar-toggle"
-                    data-uk-navbar-toggle-icon data-uk-toggle aria-label="Menu"></a>
+                <?php if ($doc->countModules('navbar-center')) : ?>
+                <div class="uk-navbar-center">
+                    <jdoc:include type="modules" name="navbar-center" style="wmarka" />
+                </div>
+                <?php endif; ?>
+
+                <div class="uk-navbar-right">
+                    <jdoc:include type="modules" name="navbar-right" style="wmarka" />
+                    <jdoc:include type="modules" name="iconnav" style="wmarka" />
+                    
+                    <a class="uk-navbar-toggle" href="#modal-full-search" uk-search-icon uk-toggle></a>
+                    
+                    <a href="#offcanvas-menu" class="uk-navbar-toggle uk-hidden@m" uk-navbar-toggle-icon uk-toggle></a>
+                </div>
+
             </div>
-            <div class="uk-navbar-right uk-visible@m">
-                <jdoc:include type="modules" name="navbar-right" style="master3" />
-            </div>		
-            <?php } ?>
-			<?php if ($_this->countModules('lang')) { ?>
-            <div class="uk-navbar-right uk-inline">
-                <jdoc:include type="modules" name="lang" style="master3" />
-			</div>	
-            <?php } ?>	
-			<div class="nav-overlay uk-navbar-right">
-			        <a class="uk-navbar-toggle" href="#modal-full-search" uk-search-icon uk-toggle></a>
-			</div>
-			<div class="nav-overlay uk-navbar-left" hidden>
-				<div class="uk-navbar-item uk-width-expand">
-					<div id="modal-full-search" class="uk-modal-full uk-modal" uk-modal>
-						<div class="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle" uk-height-viewport>
-							<button class="uk-modal-close-full" type="button" uk-close></button>
-                    <form class="uk-search uk-search-large" action="/search" method="get" role="search" >
-                    			<input  name="q" class="uk-search-input uk-text-center" type="text" 
-                    			       placeholder="Найти..." aria-label="Найти" autofocus>
-                    </form>					
-						</div>
-					</div>
-				</div>						
-			</div>			
+        </div>
+    </nav>
+</div>
+
+<div id="modal-full-search" class="uk-modal-full" uk-modal>
+    <div class="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle" uk-height-viewport>
+        <button class="uk-modal-close-full" type="button" uk-close></button>
+        <div class="uk-modal-body uk-padding-large uk-width-1-2@m">
+            <form class="uk-search uk-search-navbar uk-width-1-1" action="<?php echo Route::_('index.php?option=com_finder&view=search'); ?>" method="get">
+                <input name="q" class="uk-search-input uk-text-center" type="search" 
+                       placeholder="<?php echo Text::_('TPL_WMARKA_SEARCH_INPUT'); ?>" autofocus>
+            </form>
+            <p class="uk-text-meta uk-text-center uk-margin-small-top">
+                <?php echo Text::_('TPL_WMARKA_SEARCH_HINT'); ?>
+            </p>
         </div>
     </div>
 </div>
-<?php } ?>
-<?php
-/*
- * offcanvas
- */
-if ($_this->countModules('offcanvas')) { ?>
-<aside id="offcanvas" data-uk-offcanvas="mode:slide;overlay:true">
-    <div class="uk-offcanvas-bar">
-        <a class="uk-offcanvas-close" data-uk-close aria-label="<?php echo Text::_('JLIB_HTML_BEHAVIOR_CLOSE'); ?>"></a>
-        <jdoc:include type="modules" name="offcanvas" style="master3" />
-    </div>
-</aside>
-<?php } ?>
+<?php endif; ?>
 
-
-<?php
-/*
- * offcanvas-menu
- */
-if ($_this->countModules('offcanvas-menu')) { ?>
-<aside id="offcanvas-menu" data-uk-offcanvas="mode:slide;overlay:true">
-    <div class="uk-offcanvas-bar">
-        <a class="uk-offcanvas-close" data-uk-close aria-label="<?php echo Text::_('JLIB_HTML_BEHAVIOR_CLOSE'); ?>"></a>
-        <jdoc:include type="modules" name="offcanvas-menu" style="master3" />
-    </div>
-</aside>
-<?php } ?>
+<?php // Универсальный Offcanvas
+foreach (['offcanvas', 'offcanvas-menu'] as $name) :
+    if ($doc->countModules($name)) : ?>
+    <aside id="<?php echo $name; ?>" uk-offcanvas="mode: slide; overlay: true">
+        <div class="uk-offcanvas-bar">
+            <button class="uk-offcanvas-close" type="button" uk-close></button>
+            <jdoc:include type="modules" name="<?php echo $name; ?>" style="wmarka" />
+        </div>
+    </aside>
+    <?php endif;
+endforeach; ?>
