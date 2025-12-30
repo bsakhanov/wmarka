0cf53770e925e73cfa0b9f52a36dcc06a58d5630
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-30 16:49:00 +0500

    Update Helper.php
    добавление функций

--- php/Helper.php
+++ php/Helper.php
@@ -5,41 +5,81 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Language\Text;
 
-class Helper {
-    protected $doc;
+/**
+ * Хелпер шаблона Wmarka для Joomla 6
+ * Оптимизирован под PHP 8.3/8.4
+ */
+class Helper
+{
+    /** @var \Joomla\CMS\Document\HtmlDocument */
+    public $doc;
 
-    public function __construct($doc) {
+    /**
+     * Конструктор хелпера
+     * @param object $doc Объект документа из index.php ($this)
+     */
+    public function __construct(object $doc)
+    {
         $this->doc = $doc;
     }
 
-    // Метод для подключения частей шаблона из папки /partial/
-    public function partial($file) {
-        $path = JPATH_THEMES . '/' . $this->doc->template . '/partial/' . $file;
-        if (file_exists($path)) {
-            include $path;
-        }
-        return '';
-    }
-
-    // Формирование классов для body
-    public function getBodyClasses() {
-        $app = Factory::getApplication();
-        $menu = $app->getMenu()->getActive();
-        $cls = [];
-
-        $cls[] = 'site-' . ($app->getTemplate() ?: 'default');
-        $cls[] = 'itemid-' . ($menu ? $menu->id : '0');
-        
-        if ($menu && $menu->home) {
-            $cls[] = 'is-home';
-        }
-
-        return implode(' ', $cls);
-    }
-
-    // Заглушка для совместимости с нашим index.php
-    public function renderHTML() {
+    /**
+     * Рендеринг тега <html>
+     * Используется в index.php: <?php echo $tpl->renderHTML(); ?>
+     */
+    public function renderHTML(): string
+    {
         return '<!DOCTYPE html><html lang="' . $this->doc->language . '" dir="' . $this->doc->direction . '">';
+    }
+
+    /**
+     * Генерация классов для тега <body>
+     * Учитывает ID меню, текущий компонент и статус главной страницы
+     */
+    public function getBodyClasses(): string
+    {
+        $app   = Factory::getApplication();
+        $menu  = $app->getMenu()->getActive();
+        $input = $app->input;
+
+        $classes = [];
+        
+        // Базовые классы Joomla
+        $classes[] = 'site-' . $this->doc->template;
+        $classes[] = 'option-' . str_replace('com_', '', $input->get('option', ''));
+        $classes[] = 'view-' . $input->get('view', '');
+        $classes[] = 'layout-' . $input->get('layout', 'default');
+        $classes[] = 'itemid-' . ($menu ? $menu->id : '0');
+
+        // Класс для главной страницы
+        if (Uri::current() === Uri::base()) {
+            $classes[] = 'is-home';
+        }
+
+        // Адаптивный класс UIkit (опционально)
+        $classes[] = 'uk-offcanvas-content';
+
+        return implode(' ', array_filter($classes));
+    }
+
+    /**
+     * Подключение частей шаблона (partials)
+     * Ищет файлы в папке /templates/wmarka/partial/
+     * @param string $file Имя файла (например, 'navbar.php')
+     */
+    public function partial(string $file): string
+    {
+        $path = JPATH_THEMES . '/' . $this->doc->template . '/partial/' . $file;
+
+        if (file_exists($path)) {
+            // Буферизация вывода для корректного внедрения в index.php
+            ob_start();
+            include $path;
+            return ob_get_clean();
+        }
+
+        return '';
     }
 }
