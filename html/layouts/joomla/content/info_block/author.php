5a6cfc51afbb6cd0b02e261bd5f03df9244fea25
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 09:39:33 +0500

    Update author.php

--- html/layouts/joomla/content/info_block/author.php
+++ html/layouts/joomla/content/info_block/author.php
@@ -1,37 +1,17 @@
 <?php
-
-/**
- * @package     Joomla.Site
- * @subpackage  Layout
- *
- * @copyright   (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-\defined('_JEXEC') or die;
-
-use Joomla\CMS\Factory;
+defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
-$jsIcons = Factory::getContainer()
-    ->get(Joomla\CMS\Application\SiteApplication::class)
-    ->getTemplate(true)
-    ->params
-    ->get('jsIcons', 'none') != 'none';
-
+$item   = $displayData['item'];
+$params = $displayData['params'];
+$authorName = ($item->created_by_alias ?: $item->author);
 ?>
-<dd class="uk-flex uk-flex-middle createdby" itemprop="author" itemscope itemtype="https://schema.org/Person">
-    <?php
-    if ($jsIcons) {
-        echo '<span data-uk-icon="icon:user" aria-hidden="true"></span>&nbsp;';
-    }
-    $author = ($displayData['item']->created_by_alias ?: $displayData['item']->author);
-    $author = '<span itemprop="name">' . $author . '</span>';
-    if (!empty($displayData['item']->contact_link) && $displayData['params']->get('link_author') == true) {
-        echo '<span>' . Text::sprintf('COM_CONTENT_WRITTEN_BY', HTMLHelper::_('link', $displayData['item']->contact_link, $author, array('itemprop' => 'url'))) . '</span>';
-    } else {
-        echo '<span>' . Text::sprintf('COM_CONTENT_WRITTEN_BY', $author) . '</span>';
-    }
-    ?>
-</dd>
+<span itemprop="author" itemscope itemtype="https://schema.org/Person">
+    <?php $author = '<span itemprop="name">' . $authorName . '</span>'; ?>
+    <?php if (!empty($item->contact_link) && $params->get('link_author')) : ?>
+        <?php echo Text::sprintf('COM_CONTENT_WRITTEN_BY', HTMLHelper::_('link', $item->contact_link, $author, ['itemprop' => 'url'])); ?>
+    <?php else : ?>
+        <?php echo Text::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+    <?php endif; ?>
+</span>
