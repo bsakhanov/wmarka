c6a062da1f428bacc8943fd7e77d248396335fd0
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 16:22:31 +0500

    Update default_children.php

--- html/com_content/category/default_children.php
+++ html/com_content/category/default_children.php
@@ -1,13 +1,4 @@
 <?php
-
-/**
- * @package     Joomla.Site
- * @subpackage  com_content
- *
- * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
@@ -15,68 +6,59 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 
-$lang   = $this->getLanguage();
+/** @var \Joomla\Component\Content\Site\View\Category\HtmlView $this */
 $user   = $this->getCurrentUser();
 $groups = $user->getAuthorisedViewLevels();
-?>
 
-<?php if (count($this->children[$this->category->id]) > 0) : ?>
-    <?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
-        <?php // Check whether category access level allows access to subcategories. ?>
-        <?php if (in_array($child->access, $groups)) : ?>
-            <?php if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren())) : ?>
-            <div class="com-content-category__children">
-                <?php if ($lang->isRtl()) : ?>
-                <h3 class="page-header item-title">
-                    <?php if ($this->params->get('show_cat_num_articles', 1)) : ?>
-                        <span class="badge bg-info tip hasTooltip" title="<?php echo HTMLHelper::_('tooltipText', 'COM_CONTENT_NUM_ITEMS'); ?>">
-                            <?php echo $child->getNumItems(true); ?>
-                        </span>
-                    <?php endif; ?>
-                    <a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>">
-                    <?php echo $this->escape($child->title); ?></a>
+if (count($this->children[$this->category->id]) > 0) : ?>
+    <div class="uk-margin-medium-top" itemscope itemtype="https://schema.org/ItemList">
+        <?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
+            <?php if (in_array($child->access, $groups)) : ?>
+                <?php if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren())) : ?>
+                    
+                    <div class="uk-card uk-card-default uk-card-small uk-margin-small-bottom uk-border-rounded">
+                        <div class="uk-card-header uk-flex uk-flex-middle uk-flex-between">
+                            <h3 class="uk-card-title uk-margin-remove uk-text-bold">
+                                <a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>" class="uk-link-reset">
+                                    <?php echo $this->escape($child->title); ?>
+                                </a>
+                                <?php if ($this->params->get('show_cat_num_articles', 1)) : ?>
+                                    <span class="uk-badge uk-margin-small-left" title="<?php echo Text::_('COM_CONTENT_NUM_ITEMS'); ?>">
+                                        <?php echo $child->getNumItems(true); ?>
+                                    </span>
+                                <?php endif; ?>
+                            </h3>
 
-                    <?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
-                        <a href="#category-<?php echo $child->id; ?>" data-bs-toggle="collapse" class="btn btn-sm float-end" aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"><span class="icon-plus" aria-hidden="true"></span></a>
-                    <?php endif; ?>
-                </h3>
-                <?php else : ?>
-                <h3 class="page-header item-title"><a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>">
-                    <?php echo $this->escape($child->title); ?></a>
-                    <?php if ($this->params->get('show_cat_num_articles', 1)) : ?>
-                        <span class="badge bg-info tip hasTooltip" title="<?php echo HTMLHelper::_('tooltipText', 'COM_CONTENT_NUM_ITEMS'); ?>">
-                            <?php echo $child->getNumItems(true); ?>
-                        </span>
-                    <?php endif; ?>
-
-                    <?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
-                        <a href="#category-<?php echo $child->id; ?>" data-bs-toggle="collapse" class="btn btn-sm float-end" aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"><span class="icon-plus" aria-hidden="true"></span></a>
-                    <?php endif; ?>
-                </h3>
-                <?php endif; ?>
-                <?php if ($this->params->get('show_subcat_desc') == 1) : ?>
-                    <?php if ($child->description) : ?>
-                        <div class="category-desc">
-                            <?php echo HTMLHelper::_('content.prepare', $child->description, '', 'com_content.category'); ?>
+                            <?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
+                                <button class="uk-button uk-button-text" type="button" 
+                                        uk-toggle="target: #child-<?php echo $child->id; ?>; animation: uk-animation-fade">
+                                    <span uk-icon="icon: plus; ratio: 0.8"></span>
+                                </button>
+                            <?php endif; ?>
                         </div>
-                    <?php endif; ?>
-                <?php endif; ?>
 
-                <?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
-                <div class="collapse fade" id="category-<?php echo $child->id; ?>">
-                    <?php
-                    $this->children[$child->id] = $child->getChildren();
-                    $this->category = $child;
-                    $this->maxLevel--;
-                    echo $this->loadTemplate('children');
-                    $this->category = $child->getParent();
-                    $this->maxLevel++;
-                    ?>
-                </div>
-                <?php endif; ?>
+                        <?php if ($this->params->get('show_subcat_desc') == 1 && $child->description) : ?>
+                            <div class="uk-card-body uk-padding-small uk-text-meta">
+                                <?php echo HTMLHelper::_('content.prepare', $child->description, '', 'com_content.category'); ?>
+                            </div>
+                        <?php endif; ?>
 
-            </div>
+                        <?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
+                            <div id="child-<?php echo $child->id; ?>" class="uk-card-footer uk-background-muted" hidden>
+                                <?php
+                                $this->children[$child->id] = $child->getChildren();
+                                $this->category = $child;
+                                $this->maxLevel--;
+                                echo $this->loadTemplate('children');
+                                $this->category = $child->getParent();
+                                $this->maxLevel++;
+                                ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+
+                <?php endif; ?>
             <?php endif; ?>
-        <?php endif; ?>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 <?php endif; ?>
