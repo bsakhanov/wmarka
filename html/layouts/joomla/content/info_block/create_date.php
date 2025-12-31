cd3042eea8fd422212d9cdac0e1ff4aec33d45e2
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 09:40:49 +0500

    Update create_date.php

--- html/layouts/joomla/content/info_block/create_date.php
+++ html/layouts/joomla/content/info_block/create_date.php
@@ -6,6 +6,6 @@ use Joomla\CMS\Language\Text;
 
 $item = $displayData['item'];
 ?>
-<time datetime="<?php echo HTMLHelper::_('date', $item->created, 'c'); ?>">
-    <?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', HTMLHelper::_('date', $item->created, Text::_('DATE_FORMAT_LC3'))); ?>
+<time datetime="<?php echo Joomla\CMS\HTML\HTMLHelper::_('date', $displayData['item']->created, 'c'); ?>" itemprop="dateCreated">
+    <?php echo Joomla\CMS\Language\Text::sprintf('COM_CONTENT_CREATED_DATE_ON', Joomla\CMS\HTML\HTMLHelper::_('date', $displayData['item']->created, Joomla\CMS\Language\Text::_('DATE_FORMAT_LC3'))); ?>
 </time>
