433a95f341ce195d3c535e8bc7a275790d1545a1
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 09:41:16 +0500

    Update modify_date.php

--- html/layouts/joomla/content/info_block/modify_date.php
+++ html/layouts/joomla/content/info_block/modify_date.php
@@ -6,6 +6,6 @@ use Joomla\CMS\Language\Text;
 
 $item = $displayData['item'];
 ?>
-<time datetime="<?php echo HTMLHelper::_('date', $item->modified, 'c'); ?>">
-    <?php echo Text::sprintf('COM_CONTENT_LAST_UPDATED', HTMLHelper::_('date', $item->modified, Text::_('DATE_FORMAT_LC3'))); ?>
+<time datetime="<?php echo Joomla\CMS\HTML\HTMLHelper::_('date', $displayData['item']->modified, 'c'); ?>" itemprop="dateModified">
+    <?php echo Joomla\CMS\Language\Text::sprintf('COM_CONTENT_LAST_UPDATED', Joomla\CMS\HTML\HTMLHelper::_('date', $displayData['item']->modified, Joomla\CMS\Language\Text::_('DATE_FORMAT_LC3'))); ?>
 </time>
