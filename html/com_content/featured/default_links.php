b8d7fb52eb8b5f64706f6b6b273a89b0f9084459
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 16:35:50 +0500

    Update default_links.php

--- html/com_content/featured/default_links.php
+++ html/com_content/featured/default_links.php
@@ -1,25 +1,19 @@
 <?php
-
-/**
- * @package     Joomla.Site
- * @subpackage  com_content
- *
- * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
-
 ?>
-<ul class="uk-list uk-margin-top links">
-    <?php foreach ($this->link_items as $item) { ?>
-        <li class="link">
-            <a href="<?php echo Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language)); ?>">
-                <?php echo $item->title; ?>
-            </a>
-        </li>
-    <?php } ?>
-</ul>
+<div class="uk-card uk-card-secondary uk-card-small uk-border-rounded uk-padding-small">
+    <h3 class="uk-heading-bullet uk-margin-small-bottom uk-text-small uk-text-uppercase"><?php echo Joomla\CMS\Language\Text::_('COM_CONTENT_MORE_ARTICLES'); ?></h3>
+    <ul class="uk-list uk-list-divider uk-margin-remove-bottom">
+        <?php foreach ($this->link_items as $item) : ?>
+            <li>
+                <a href="<?php echo Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language)); ?>" class="uk-link-reset uk-flex uk-flex-middle">
+                    <span uk-icon="icon: chevron-right; ratio: 0.8" class="uk-margin-small-right"></span>
+                    <span class="uk-text-small"><?php echo $this->escape($item->title); ?></span>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
