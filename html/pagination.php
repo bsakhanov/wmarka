f7dc1c3dcdc9463a4b02f80746eac4a1cac03bea
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 15:31:05 +0500

    Update pagination.php
    WMARKA ULTRA (UIkit 3 Pagination + Full Microdata)

--- html/pagination.php
+++ html/pagination.php
@@ -1,65 +1,76 @@
 <?php
-defined('_JEXEC') or die;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 /**
- * Рендеринг списка страниц (Обертка)
+ * @package     Joomla.Site
+ * @subpackage  Template.wmarka
+ * @version     WMARKA ULTRA (UIkit 3 Pagination + Full Microdata)
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Рендерит основной контейнер пагинации
  */
 function pagination_list_render($list): string
 {
-    // Если страниц нет — ничего не выводим
-    if (empty($list['pages'])) {
-        return '';
+    // Обертка навигации с микроразметкой SiteNavigationElement
+    $html = '<nav aria-label="' . Joomla\CMS\Language\Text::_('JLIB_HTML_PAGINATION') . '">';
+    $html .= '<ul class="uk-pagination uk-flex-center uk-margin-large-top" uk-margin itemscope itemtype="https://schema.org/SiteNavigationElement">';
+
+    if ($list['start']['active']) {
+        $html .= $list['start']['data'];
+    }
+    if ($list['previous']['active']) {
+        $html .= $list['previous']['data'];
     }
 
-    $html = '<ul class="uk-pagination uk-flex-center uk-margin-large-top" uk-margin>';
-
-    // Кнопки "В начало" и "Назад"
-    $html .= $list['start']['data'];
-    $html .= $list['previous']['data'];
-
-    // Номера страниц
     foreach ($list['pages'] as $page) {
         $html .= $page['data'];
     }
 
-    // Кнопки "Вперед" и "В конец"
-    $html .= $list['next']['data'];
-    $html .= $list['end']['data'];
+    if ($list['next']['active']) {
+        $html .= $list['next']['data'];
+    }
+    if ($list['end']['active']) {
+        $html .= $list['end']['data'];
+    }
 
-    $html .= '</ul>';
+    $html .= '</ul></nav>';
 
     return $html;
 }
 
 /**
- * Рендеринг активной ссылки (другие страницы)
+ * Рендерит активную ссылку на страницу
  */
 function pagination_item_active(&$item): string
 {
-    // Определяем иконки для навигации
-    $title = $item->text;
-    
-    // Заменяем текст на иконки UIkit для крайних кнопок
-    if (str_contains($item->text, 'JLIB_HTML_PAGINATION_NEXT') || $item->text == '>') {
-        $title = '<span uk-pagination-next></span>';
-    } elseif (str_contains($item->text, 'JLIB_HTML_PAGINATION_PREV') || $item->text == '<') {
-        $title = '<span uk-pagination-previous></span>';
+    $display = $item->text;
+
+    // Заменяем текстовые метки на иконки UIkit
+    if ($item->text == Joomla\CMS\Language\Text::_('JLIB_HTML_PAGINATION_START')) {
+        $display = '<span uk-pagination-previous></span><span uk-pagination-previous></span>';
+    } elseif ($item->text == Joomla\CMS\Language\Text::_('JPREV')) {
+        $display = '<span uk-pagination-previous></span>';
+    } elseif ($item->text == Joomla\CMS\Language\Text::_('JNEXT')) {
+        $display = '<span uk-pagination-next></span>';
+    } elseif ($item->text == Joomla\CMS\Language\Text::_('JLIB_HTML_PAGINATION_END')) {
+        $display = '<span uk-pagination-next></span><span uk-pagination-next></span>';
     }
 
-    return '<li><a href="' . $item->link . '" aria-label="' . htmlspecialchars($item->text) . '">' . $title . '</a></li>';
+    // Внедряем itemprop="url" и itemprop="name" для SEO
+    return '<li><a href="' . $item->link . '" itemprop="url"><span itemprop="name">' . $display . '</span></a></li>';
 }
 
 /**
- * Рендеринг неактивного элемента (текущая страница или заглушка)
+ * Рендерит текущую страницу или неактивный элемент
  */
 function pagination_item_inactive(&$item): string
 {
-    // Если это текущая страница
-    if ($item->active) {
+    // Текущая страница
+    if ($item->text == (int) $item->text || $item->active) {
         return '<li class="uk-active"><span>' . $item->text . '</span></li>';
     }
 
-    // Если это отключенные стрелки (например, "Назад" на первой странице)
+    // Отключенные кнопки (например, "Назад" на первой странице)
     return '<li class="uk-disabled"><span>' . $item->text . '</span></li>';
 }
