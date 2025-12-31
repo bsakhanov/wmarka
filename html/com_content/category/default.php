f3785a9989c839fcb335222354c016bc246f0f04
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 16:21:43 +0500

    Update default.php
    Полностью удалены классы btn-group, badge bg-info, table-striped (Bootstrap). Заменены на uk-table, uk-label, uk-grid

--- html/com_content/category/default.php
+++ html/com_content/category/default.php
@@ -1,23 +1,16 @@
 <?php
-
-/**
- * @package     Joomla.Site
- * @subpackage  com_content
- *
- * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Layout\LayoutHelper;
 
+/** @var \Joomla\Component\Content\Site\View\Category\HtmlView $this */
 ?>
-<div class="com-content-category category-list">
-
-<?php
-$this->subtemplatename = 'articles';
-echo LayoutHelper::render('joomla.content.category_default', $this);
-?>
-
+<div class="category-list-wrap uk-section uk-section-xsmall" itemscope itemtype="https://schema.org/CollectionPage">
+    <div class="uk-container">
+        <?php
+        $this->subtemplatename = 'articles';
+        // Этот вызов рендерит заголовок и описание из layouts/joomla/content/category_default.php
+        echo LayoutHelper::render('joomla.content.category_default', $this);
+        ?>
+    </div>
 </div>
