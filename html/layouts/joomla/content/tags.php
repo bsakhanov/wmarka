a4a8fd51e166d88d8f0113e64b1fde91f9bf4c06
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 08:37:14 +0500

    Update tags.php
    WMARKA UIKIT ULTRA (Joomla 6 Original Logic + uk-margin)

--- html/layouts/joomla/content/tags.php
+++ html/layouts/joomla/content/tags.php
@@ -1,43 +1,52 @@
 <?php
-
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
- *
- * @copyright   (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @version     WMARKA UIKIT ULTRA (Joomla 6 Original Logic + uk-margin)
  */
 
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Tags\Site\Helper\RouteHelper;
 use Joomla\Registry\Registry;
-JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/route.php');
 
-$app = Factory::getContainer()->get(Joomla\CMS\Application\SiteApplication::class);
-$authorised = $app->getIdentity()->getAuthorisedViewLevels();
-$jsIcons = $app->getTemplate(true)->params->get('jsIcons', 'none') != 'none';
+// Проверка уровней доступа — ничего не пропускаем из оригинала
+$authorised = Factory::getUser()->getAuthorisedViewLevels();
 
-?>
-<?php if (!empty($displayData)) { ?>
-<small>
-<p>
-    <?php
-    foreach ($displayData as $i => $tag) {
-        if (in_array($tag->access, $authorised)) {
-            $tagParams = new Registry($tag->params);
-            $link_class = $tagParams->get('tag_link_class', 'label label-info');
-            ?>
-    
-        <a href="<?php echo Route::_(TagsHelperRoute::getTagRoute($tag->tag_id . ':' . $tag->alias)); ?>" class="uk-link-heading <?php echo $link_class; ?> margin-mini4 ">#<?php echo $this->escape($tag->title); ?></a>
-  
-    <?php
-        }
-    }
-    ?>
-</p>
-</small>
-<?php
+if (empty($displayData)) {
+    return;
 }
+?>
+
+<div uk-margin class="uk-margin-small-top uk-margin-small-bottom wmarka-tags-container">
+    <?php foreach ($displayData as $i => $tag) : ?>
+        <?php 
+        // 1. Проверка прав доступа (как в оригинале)
+        if (!in_array($tag->access, $authorised)) continue; 
+
+        // 2. Логика фильтрации "Эксклюзива" (наша фишка)
+        if (mb_strtolower(trim($tag->title)) === 'эксклюзив') continue; 
+
+        // 3. Получение параметров тега (классы из админки)
+        $tagParams  = new Registry($tag->params);
+        $customClass = $tagParams->get('tag_link_class', 'uk-button-default');
+        
+        // Маппинг Bootstrap классов в UIkit (если в админке выбраны старые классы)
+        $btnClass = str_replace(['btn-info', 'btn-primary', 'btn-success', 'btn-sm'], ['uk-button-primary', 'uk-button-primary', 'uk-button-secondary', 'uk-button-small'], $customClass);
+        
+        // Гарантируем наличие базового класса кнопки UIkit
+        if (strpos($btnClass, 'uk-button') === false) {
+            $btnClass .= ' uk-button uk-button-default';
+        }
+        ?>
+
+        <a href="<?php echo Route::_(RouteHelper::getComponentTagRoute($tag->tag_id . ':' . $tag->alias, $tag->language)); ?>" 
+           class="<?php echo trim($btnClass); ?> uk-button-small uk-border-rounded tag-<?php echo $tag->tag_id; ?>">
+            <span uk-icon="icon: tag; ratio: 0.7" class="uk-margin-xsmall-right"></span>
+            <?php echo $this->escape($tag->title); ?>
+        </a>
+
+    <?php endforeach; ?>
+</div>
