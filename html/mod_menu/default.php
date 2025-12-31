b1eb5601f9332aa06f91bd4422146aee5d8fe674
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2025-12-31 14:58:58 +0500

    Update default.php
    WMARKA ULTRA (UIkit 3 Navbar + Schema.org)

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -2,7 +2,7 @@
 /**
  * @package     Joomla.Site
  * @subpackage  mod_menu
- * @version     WMARKA ULTRA (UIkit 3 Navbar)
+ * @version     WMARKA ULTRA (UIkit 3 Navbar + Schema.org)
  */
 
 defined('_JEXEC') or die;
@@ -17,20 +17,21 @@ if ($params->get('tag_id')) {
 $attributes['class'] = 'uk-navbar-nav ' . $params->get('class_sfx');
 ?>
 
-<ul <?php echo HTMLHelper::_('itags', $attributes); ?>>
+<ul <?php echo HTMLHelper::_('itags', $attributes); ?> itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php foreach ($list as $i => &$item) : ?>
     <?php
     $item_class = ['item-' . $item->id];
+    
+    // UIkit классы состояния
     if ($item->id == $active_id) $item_class[] = 'uk-active';
     if ($item->parent) $item_class[] = 'uk-parent';
     
-    // Определяем наличие дочерних элементов
     $hasChildren = $item->deeper;
     ?>
     
     <li class="<?php echo implode(' ', $item_class); ?>">
         <?php
-        // Выбор макета в зависимости от типа ссылки
+        // Выбор макета. Внутри этих файлов мы уже добавили itemprop="url" и itemprop="name"
         switch ($item->type) :
             case 'separator':
             case 'component':
@@ -43,12 +44,13 @@ $attributes['class'] = 'uk-navbar-nav ' . $params->get('class_sfx');
                 break;
         endswitch;
 
-        // Если есть вложенные пункты
+        // Вложенные пункты (Dropdown)
         if ($hasChildren) : ?>
             <div class="uk-navbar-dropdown" uk-dropdown="offset: 0; pos: bottom-left; boundary: !.uk-navbar; animation: uk-animation-slide-top-small; duration: 200">
                 <ul class="uk-nav uk-navbar-dropdown-nav">
         <?php endif; ?>
 
+    <?php /* Закрытие тегов при уменьшении уровня вложенности */ ?>
     <?php if ($item->shallower) : ?>
         <?php echo str_repeat('</ul></div></li>', $item->level_diff); ?>
     <?php elseif (!$hasChildren) : ?>
