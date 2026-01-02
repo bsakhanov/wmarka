830a55670154a710b165e6a5d8eaf3562dc07d82
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2026-01-02 23:34:16 +0500

    Update default.php

--- html/com_content/category/default.php
+++ html/com_content/category/default.php
@@ -5,12 +5,12 @@ use Joomla\CMS\Layout\LayoutHelper;
 
 /** @var \Joomla\Component\Content\Site\View\Category\HtmlView $this */
 ?>
-<div class="category-list-wrap uk-section uk-section-xsmall" itemscope itemtype="https://schema.org/CollectionPage">
-    <div class="uk-container">
+<div itemscope itemtype="https://schema.org/CollectionPage">
+
         <?php
         $this->subtemplatename = 'articles';
         // Этот вызов рендерит заголовок и описание из layouts/joomla/content/category_default.php
         echo LayoutHelper::render('joomla.content.category_default', $this);
         ?>
-    </div>
+
 </div>
