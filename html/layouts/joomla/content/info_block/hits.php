827307e43908fd7c682ac7c43380780db91e22e6
Beibit Sakhanov <45456925+bsakhanov@users.noreply.github.com>
2026-03-29 18:08:04 +0500

    Delete html directory

--- html/layouts/joomla/content/info_block/hits.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-use Joomla\CMS\Language\Text;
-$item = $displayData['item'];
-?>
-<meta itemprop="interactionCount" content="UserPageVisits:<?php echo $item->hits; ?>">
-<span class="hits-count">
-    <?php echo Text::sprintf('COM_CONTENT_ARTICLE_HITS', $item->hits); ?>
-</span>
